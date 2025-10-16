f75e081447da2a8932d9eb4fbf1f1a406789e062
Bobby-Math <bobbyjosephmathews@gmail.com>
2025-10-16 19:18:10 +0530

    feat: Implement IMDSv2 support for EC2 metadata service
         The previous implementation was using IMDSv1, which was causing a `401 Unauthorized` error when running on an EC2
          instance configured to use IMDSv2.
    
         This commit updates the `check_for_notice` function to support IMDSv2 by:
    
         - First fetching a session token from the token URL (`http://169.254.169.254/latest/api/token`).
         - Using the session token in the `X-aws-ec2-metadata-token` header of the request to the metadata URL.
    
        This ensures that the program can successfully authenticate with the EC2 metadata service and check for spot instance
          interruption notices.

--- my_spot_serve_workspace/spot_sentinel/src/main.rs
+++ my_spot_serve_workspace/spot_sentinel/src/main.rs
@@ -1,8 +1,10 @@
-use reqwest::{StatusCode,get}; 
-use serde::{Serialize, Deserialize}; 
-use std::time::Duration; 
+
+use reqwest::{Client, StatusCode};
+use serde::{Deserialize, Serialize};
+use std::time::Duration;
 
 const METADATA_URL: &str = "http://169.254.169.254/latest/meta-data/spot/instance-action";
+const TOKEN_URL: &str = "http://169.254.169.254/latest/api/token";
 
 #[derive(Serialize, Deserialize, Debug)]
 struct InterruptNotice {
@@ -23,22 +25,44 @@ async fn main() {
 }
 
 async fn check_for_notice() -> Result<(), reqwest::Error> {
+    let client = Client::new();
 
-        let response = get(METADATA_URL).await?;
+    // Get a session token
+    let token_response = client
+        .put(TOKEN_URL)
+        .header("X-aws-ec2-metadata-token-ttl-seconds", "21600")
+        .send()
+        .await?;
 
-        match response.status() {
-            StatusCode::OK => {
-                println!("Interruption Notice Found");
-                let notice = response.json::<InterruptNotice>().await?;
-                println!("Notice Details:{:?}",notice);
-            }
-            StatusCode::NOT_FOUND => {
-                println!("No InterruptNotice");
-            }
-            other =>  { 
-                println!("Unexpected Status Code, {other}"); 
-            }
+    if token_response.status() != StatusCode::OK {
+        println!(
+            "Unexpected status code when getting token: {}",
+            token_response.status()
+        );
+        return Ok(());
+    }
+
+    let token = token_response.text().await?;
+
+    // Check for interruption notice
+    let response = client
+        .get(METADATA_URL)
+        .header("X-aws-ec2-metadata-token", token)
+        .send()
+        .await?;
+
+    match response.status() {
+        StatusCode::OK => {
+            println!("Interruption Notice Found");
+            let notice = response.json::<InterruptNotice>().await?;
+            println!("Notice Details:{:?}", notice);
         }
-            Ok(())
-
+        StatusCode::NOT_FOUND => {
+            println!("No InterruptNotice");
+        }
+        other => {
+            println!("Unexpected Status Code, {other}");
+        }
+    }
+    Ok(())
 }
